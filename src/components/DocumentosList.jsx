36f671aa9dc77d876c5d18b2e910ed7f1cba6cf1
Luciano Bustos <bustosluciano89@gmail.com>
2025-06-05 20:28:13 -0300

    feat: Refactor documentos page to use DocumentosList component for data rendering

---
+++ src/components/DocumentosList.jsx
@@ -0,0 +1,25 @@
+// src/components/DocumentosList.jsx
+import { supabase } from '../lib/supabaseClient';
+import { useEffect, useState } from 'react';
+
+export default function DocumentosList() {
+  const [documentos, setDocumentos] = useState([]);
+
+  useEffect(() => {
+    supabase
+      .from('documentos')
+      .select('*')
+      .then(({ data }) => setDocumentos(data));
+  }, []);
+
+  return (
+    <ul>
+      {documentos.map((doc) => (
+        <li key={doc.id}>
+          <h2>{doc.titulo}</h2>
+          <div dangerouslySetInnerHTML={{ __html: doc.contenido }} />
+        </li>
+      ))}
+    </ul>
+  );
+}
